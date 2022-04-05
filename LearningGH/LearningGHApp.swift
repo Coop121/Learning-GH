046a943e83700cca47830af179f65c994b77e1af
Cooper Clarke <cooper.clarke@ryerson.ca>
2022-04-05 17:59:33 -0400

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Cooper Clarke on 2022-04-05.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
